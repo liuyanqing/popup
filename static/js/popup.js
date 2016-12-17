b6fe9bde24e8eade5eca92115c4f108b7647994e
yanqing.liu <2604389912@qq.com>
2016-12-17 14:16:10 +0800

    bug

--- static/js/popup.js
+++ static/js/popup.js
@@ -16,37 +16,44 @@ var clearHideClass = function(classObj){
 	return classObjNew.join(" ");
 };
 
-var popup = function() {
+var popup = function(funOpen, funClose) {
 
 	document.onclick = function(e) {
 		var e = e ? e : window.event,
-    	tar = e.srcElement || e.target,
-    	classNa = tar.className.split(" "),
-    	obj = tar.getAttribute("data-target"),
-    	popupList = document.getElementsByClassName("popup"),
-    	popupSideList = document.getElementsByClassName("popupSide"),
-    	list = [];
+        	tar = e.srcElement || e.target,
+        	obj = tar.getAttribute("data-target");
+        if(!obj) return;
+        var classNa = tar.className.split(" "),
+        	popupList = document.getElementsByClassName("popup"),
+        	popupSideList = document.getElementsByClassName("popupSide"),
+        	list = [];
     	list = list.concat(makeArray(popupList), makeArray(popupSideList));
     	for( var i in classNa) {
+            if(classNa[i] == "popup-open") {
+                for( var k = 0,len = list.length; k < len; k++ ) {
+                    if(list[k].getAttribute("data-popup") == obj) {
+                        var classObj = list[k].className.split(" ");
+                        list[k].className = clearHideClass( classObj );
+                        if(typeof funOpen === 'function') {
+                            funOpen(obj, list[k]);
+                        }
+                        return;
+                    }
+                }
+                return;
+            }
 			if(classNa[i] == "popup-dismissed") {
 				for( var j = 0,len = list.length; j < len; j++ ) {
 					if(list[j].getAttribute("data-popup") == obj) {
 						list[j].className += " hide";
+                        if(typeof funClose === 'function') {
+                            funClose(obj, list[k]);
+                        }
 						return;
 					}
 				}
 				return;
 			};
-			if(classNa[i] == "popup-open") {
-				for( var k = 0,len = list.length; k < len; k++ ) {
-					if(list[k].getAttribute("data-popup") == obj) {
-						var classObj = list[k].className.split(" ");
-						list[k].className = clearHideClass( classObj );
-						return;
-					}
-				}
-				return;
-			}
     	}
 
 	}
