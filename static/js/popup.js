7b85df0d0f95a9f8d69aabac998b6f9681648c78
yanqing.liu <2604389912@qq.com>
2016-12-22 11:09:35 +0800

    pop

--- static/js/popup.js
+++ static/js/popup.js
@@ -2,61 +2,71 @@
 var makeArray = function(obj){
     var res = [];
     for(var i = 0,len = obj.length; i < len; i++ ){
-    	res.push(obj[i]);
+        res.push(obj[i]);
     }
     return res;
 };
 var clearHideClass = function(classObj){
-	var classObjNew = [];
-	for ( var n = 0,len = classObj.length; n < len; n++) {
-		if( classObj[n] != "hide"){
-			classObjNew.push(classObj[n]);
-		}
-	}
-	return classObjNew.join(" ");
+    var classObjNew = [];
+    for ( var n = 0,len = classObj.length; n < len; n++) {
+        if( classObj[n] != "hide"){
+            classObjNew.push(classObj[n]);
+        }
+    }
+    return classObjNew.join(" ");
 };
 
 var popup = function(funOpen, funClose) {
 
-	document.onclick = function(e) {
-		var e = e ? e : window.event,
-        	tar = e.srcElement || e.target,
-        	obj = tar.getAttribute("data-target");
+    document.onclick = function(e) {
+        var e = e ? e : window.event,
+            tar = e.srcElement || e.target,
+            tarName = tar.tagName.toLowerCase(),
+            classNa;
+        if(tarName === 'svg') {
+            tar = tar.parentNode;
+        } else if(tarName === 'use') {
+            tar = tar.parentNode.parentNode;
+        }
+        var obj = tar.getAttribute("data-target");
         if(!obj) return;
-        var classNa = tar.className.split(" "),
-        	popupList = document.getElementsByClassName("popup"),
-        	popupSideList = document.getElementsByClassName("popupSide"),
-        	list = [];
-    	list = list.concat(makeArray(popupList), makeArray(popupSideList));
-    	for( var i in classNa) {
+        e.stopPropagation();
+        e.preventDefault();
+        classNa = tar.className.split(" ");
+        // tar.tagName.toLowerCase() !== "svg"?tar.className.split(" "):tar.className.animVal.split(" ");
+        var popupList = document.getElementsByClassName("popup"),
+            popupSideList = document.getElementsByClassName("popupSide"),
+            list = [];
+        list = list.concat(makeArray(popupList), makeArray(popupSideList));
+        for( var i in classNa) {
             if(classNa[i] == "popup-open") {
                 for( var k = 0,len = list.length; k < len; k++ ) {
                     if(list[k].getAttribute("data-popup") == obj) {
                         var classObj = list[k].className.split(" ");
                         list[k].className = clearHideClass( classObj );
                         if(typeof funOpen === 'function') {
-                            funOpen(obj, list[k]);
+                            funOpen(obj, list[k], tar);
                         }
                         return;
                     }
                 }
                 return;
             }
-			if(classNa[i] == "popup-dismissed") {
-				for( var j = 0,len = list.length; j < len; j++ ) {
-					if(list[j].getAttribute("data-popup") == obj) {
-						list[j].className += " hide";
+            if(classNa[i] == "popup-dismissed") {
+                for( var j = 0,len = list.length; j < len; j++ ) {
+                    if(list[j].getAttribute("data-popup") == obj) {
+                        list[j].className += " hide";
                         if(typeof funClose === 'function') {
-                            funClose(obj, list[k]);
+                            funClose(obj, list[j], tar);
                         }
-						return;
-					}
-				}
-				return;
-			};
-    	}
+                        return;
+                    }
+                }
+                return;
+            };
+        }
 
-	}
+    }
 };
 window.popup = popup;
 
